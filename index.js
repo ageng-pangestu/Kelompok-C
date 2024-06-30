9ddf9599e3a78acc2e25032fefb38180212377b7
Ageng Pangestu <agengpangstu99@gmail.com>
2024-06-30 17:26:36 +0700

    Soal-3

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ cetakPola(5);
 
 function cetakPola(height) {
   for (let i = 0; i < height; i++) {
+    //ini umtuk bikin bentuk segitiganya rapih
     let space = "";
     for (let j = 0; j < i; j++) {
       space += " ";
     }
 
+    //ini untuk segitiga kebawah dan kondisi
     let symbol = "";
     if (i % 2 == 0) {
-      for (let j = 0; j < i; j++) {
-        if (j % 2 === 0) {
+      for (let k = i; k < height; k++) {
+        if (k % 2 === 0) {
           symbol = symbol + "# ";
         } else {
           symbol = symbol + "+ ";
         }
       }
     } else {
-      for (let j = 0; j < i; j++) {
+      for (let m = i; m < height; m++) {
         symbol = symbol + "+ ";
       }
     }
